bd7266329e711e76bd79d9fee947e5da642940b7
lcadena <36576978+lcadena@users.noreply.github.com>
2018-02-18 02:53:21 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
 
     public static void main(Figura[] l) {
-	// write your code here
+	// funcion suma
 	double ret=0;
-	    for(Figura:l){
+	for(Figura:l){
 	      ret+=l.calculArea();
-	    }
+	}
+	//ver area circulo     
+        Cercle cercle = new Cercle();
+        cercle.calculArea();
+        System.out.println("Area del cercle: " + cercle.calculArea());
     }
 }
